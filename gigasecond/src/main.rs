f0af436d421e788d2892e11883f692e72819f411
ALsJourney <aldin.dino@outlook.com>
2023-07-28 08:53:45 +0200

    Finished gigaseconds

--- gigasecond/src/main.rs
+++ gigasecond/src/main.rs
@@ -1,17 +1,16 @@
-use time::{date, time, Duration, PrimitiveDateTime as DateTime};
-
+use time::{Date, Time};
+use time::{Duration, PrimitiveDateTime as DateTime};
 fn main() {
-    let birthdate = DateTime::new(date!(2015 - 01 - 24), time!(22:00:00));
-    let date_with_gigasecond_added = after(birthdate);
-    println!("{}", date_with_gigasecond_added);
+    let test = DateTime::new(
+        Date::from_calendar_date(2000, 12.try_into().unwrap(), 28).unwrap(),
+        Time::from_hms(9, 55, 14).unwrap(),
+    );
+
+    println!("{}", after(test));
 }
 
 fn after(start: DateTime) -> DateTime {
-    // A gigasecond is one thousand million seconds.
-    // That is a one with nine zeros after it.
     let gigasecond = Duration::seconds(1_000_000_000);
-
-    let result_datetime = start + gigasecond;
-
-    result_datetime
+    //println!("{}", start + gigasecond);
+    start + gigasecond
 }
